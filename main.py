f21a652d798e830d07577de43ecde9148c27ae80
wnsurld <wnsgur00199@naver.com>
2025-11-03 19:14:36 +0900

    nas 업로드 완료

---
+++ main.py
@@ -0,0 +1,22 @@
+import os, csv
+
+def make_csv(id):
+    conn = db_connection()  #dbconn.py의 db_connection() 생성할것
+    cur = conn.cursor()
+    query = "SELECT * FROM sample_data WHERE id = %s;"
+    cur.excute(query, (id,))
+    rows = cur.fetchall()
+    column_names = [desc[0] for desc in cur.description]  #select한 객체들의 id값 추출
+
+    file_path = fr"Z:\sample_data_{id}.csv" #nas 폴더에 csv파일저장
+    with open(file_path, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer()
+        writer.writerow(column_names)
+        writer.writerows(rows)
+    print("CSV파일 변환 및 NAS 저장 완료")
+
+    cur.close()
+    conn.close()
+
+def 
+
