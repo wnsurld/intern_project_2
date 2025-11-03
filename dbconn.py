c9e3e7ca03d8f2917bbd547c71a6ab0be0d4ea14
wnsurld <wnsgur00199@naver.com>
2025-11-03 21:49:39 +0900

    마지막

---
+++ dbconn.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+import os, psycopg2
+
+load_dotenv()
+
+def db_connection():
+    conn = psycopg2.connect(
+        host = os.getenv("HOST"),
+        dbname = os.getenv("DBNAME"),
+        user = os.getenv("USER"),
+        password = os.getenv("PASSWD"),
+        port = os.getenv("PORT")
+    )
+    return conn
+    
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os, csv
+import csv
+from dbconn import db_connection
 
 id = input("업로드할 ID : ")
 
@@ -6,12 +7,12 @@ id = input("업로드할 ID : ")
 conn = db_connection()  #dbconn.py의 db_connection() 생성할것
 cur = conn.cursor()
 query = "SELECT * FROM sample_data WHERE id = %s;"
-cur.excute(query, (id,))
+cur.execute(query, (id,))
 rows = cur.fetchall()
 column_names = [desc[0] for desc in cur.description]  #select한 객체들의 id값 추출
 
 #쿼리한 데이터 csv로 변환 후 NAS에 저장
-file_path = fr"Z:\sample_data_{id}.csv" #nas 폴더에 csv파일저장
+file_path = fr"Z:\sample_data_{id}.csv" #nas 폴더에 csv파일저장(경로 다시 확인 필요)
 with open(file_path, mode='w', newline='', encoding='utf-8') as file:
     writer = csv.writer()
     writer.writerow(column_names)
