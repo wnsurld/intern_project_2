401aea678e10a6cb94fb5ec83ab76a55260147bc
wnsurld <wnsgur00199@naver.com>
2025-11-20 21:20:13 +0900

    수정

--- services/upload_to_s3.py
+++ services/upload_to_s3.py
@@ -5,7 +5,8 @@ def upload_to_s3(nas_zip_path):
     s3 = s3_connection()
     bucket_name = get_bucketName()
     filename = os.path.basename(nas_zip_path) 
-    s3_key = f"{filename}"
+    prefix = os.getenv("S3_PREFIX_IMPORT") # ~~/
+    s3_key = f"{prefix}{filename}"
 
     s3.upload_file(nas_zip_path, bucket_name, s3_key)
     print("S3 업로드 완료")
