4bc93f8d8c2b79a94165410c86d99aac36474bb1
wnsurld <wnsgur00199@naver.com>
2025-12-01 19:47:56 +0900

    최종 수정

--- uploader.py
+++ uploader.py
@@ -6,8 +6,7 @@ from services.upload_to_nas import upload_to_nas
 from services.upload_to_s3 import upload_to_s3
 from services.delete_from_nas import delete_from_nas
 
-# 현재 로컬 pc에서 수행중인데 경로를 nas로 수정
-# nas에서 구동하는 로직?
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 folder_name = input("업로드 할 DICOM 폴더 : ")
@@ -15,10 +14,10 @@ dicom_root = os.path.join(BASE_DIR, "dicom")    #다이컴 폴더 경로       -
 folder_path = os.path.join(dicom_root, folder_name)  #입력받은 다이컴 폴더 경로     -/-/dicom/input_value
 
 #nas 업로드
-nas_target = "C:/a" # 수정하기
+nas_target = "Z:/" 
 nas_folder_path = upload_to_nas(folder_path, nas_target)    #Z:/filename
 
-dicom_files = [file for file in os.listdir(nas_folder_path) if file.lower().endswith(".dcm")]   # -/-/dicom/input_value/0004.DCM~~
+dicom_files = [file for file in os.listdir(nas_folder_path) if file.lower().endswith(".dcm")] 
 first_file = os.path.join(nas_folder_path, dicom_files[0]) #폴더 내 첫번째 파일 경로
 
 dcm_header = get_header(first_file) #헤더 필드 get
